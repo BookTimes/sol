77d8de89758a80218ea67049bd2b5a3691e34915
BookTimes <102360162+BookTimes@users.noreply.github.com>
2022-11-26 12:19:56 +0400

    Update script.js

--- script.js
+++ script.js
@@ -196,5 +196,6 @@ function delc() {
     var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
     document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
   }
+    setCookie("id", "", 365);
   location.reload();
 }
