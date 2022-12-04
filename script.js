9eaef7932b3394b375f5b12042c30d846a375e29
BookTimes <102360162+BookTimes@users.noreply.github.com>
2022-12-04 17:01:33 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,204 +1,101 @@
-const qn = document.getElementById("tbox");
-const sub = document.getElementById("sel");
-const sbtn = document.getElementById("tb");
-var ol = [];
-let params = new URLSearchParams(window.location.search);
-document.all.tbox.textContent = params.has("question") ? params.get("question") : "";
-fetch("https://jsonblob.com/api/1043609418884988928")
-  .then((response) => response.json())
-  .then((json) => (ol = json));
-const user = {
-  id: getCookie("id"),
-  gr: getCookie("gr"),
-  role: getCookie("role"),
-  suj: getCookie("suj"),
-};
-document.getElementById("userId").innerHTML = user.id;
-if (user.role == "tr") {
-  document.getElementById("userGr").innerHTML = "Subject: " + user.suj;
-  document.getElementById("qnb").classList.add("hid");
-  document.getElementById("ol").innerHTML = "Doubts";
-} else {
-  document.getElementById("userGr").innerHTML = "Grade: " + user.gr;
-}
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>ask</title>
+    <link rel="stylesheet" href="style.css" />
+    <link rel="preconnect" href="https://fonts.googleapis.com" />
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link
+      href="https://fonts.googleapis.com/css2?family=Cabin:wght@600&display=swap"
+      rel="stylesheet"
+    />
+  </head>
+  <body onload="checkCookie()">
+    <div class="grid-container">
+      <div class="det">
+        <span id='userId'></span><br>
+        <span id="userGr"></span>
+               <button class="tb" onclick="delc()" style="height: 20px; font-size: small;">Sign Out</button>
 
-function submit() {
-  if (qn.value.replace(" ", "") != "") {
-    if (sub.value != "nono") {
-      subm(qn.value, sub.value, user.id, user.gr);
-      send(false, list[list.length - 1]);
-      setTimeout(function () {
-        location.reload();
-      }, 1000);
-    } else {
-    }
-  }
-}
+                </div>
 
-class qury {
-  constructor(qun, suj, us, gr, cm) {
-    (this.qun = qun),
-      (this.suj = suj),
-      (this.us = us),
-      (this.gr = gr),
-      (this.cm = cm);
-  }
-}
-
-list = [];
-
-function subm(qun, suj, us, gr, cm = []) {
-  newTask = new qury(qun, suj, us, gr, cm);
-  list.push(newTask);
-}
-
-function send(linda = true, qn = "") {
-  var url = "https://jsonblob.com/api/1043609418884988928";
-  var xhr = new XMLHttpRequest();
-  xhr.open("PUT", url);
-
-  xhr.setRequestHeader("Content-Type", "application/json");
-
-  xhr.onreadystatechange = function () {
-    if (xhr.readyState === 4) {
-    }
-  };
-  if (linda == false) {
-    ol.push(JSON.parse(JSON.stringify(qn)));
-  }
-  // qns = JSON.stringify(ol).push(JSON.stringify(qn));
-  // var data = JSON.stringify(qns);
-  console.log(qn);
-  var data = JSON.stringify(ol);
-  xhr.send(data);
-}
-var setn = 0;
-setTimeout(function () {
-  ol.forEach(function (i) {
-    var reco = "";
-    var thme = "";
-    if (user.role == "sd") {
-      if (user.gr != i.gr) {
-        reco = "hid";
-      }
-    }
-    if (user.role == "tr") {
-      if (user.suj != i.suj) {
-        reco = "hid";
-      }
-    }
-
-    const alcm = i.cm.join("<br><hr>");
-    document.getElementById("dbts").insertAdjacentHTML(
-      "afterbegin",
-      `
-      <div class="dbt ${reco} ${i.suj}">
-      <div class="dbc">
-        <span id="userId">${i.us}</span> <span class="suj">${i.suj}</span><br><br>
-                  <span id="qn">${i.qun}</span>
-          <hr>
-          <h2>Answers</h2>
-      <div class="cms">
-        <div class="cm">
-          <div class="cmt">
-          <span id='qn'>
-           ${alcm}
-              </span>
-          </div>
-          <div style="display: flex;"><textarea onclick='ent(this)' name='${setn}'  placeholder="Do you know the answer" class="cmbox sel" id="cmbox ${setn}"></textarea><button onclick='ansub(this)' id ='${setn}' button class="tb" style="  background: rgb(36, 36, 41);
-            ">Answer</button></div>
-          
-          
-        </div>
+      <div class="item1" style="position: fixed;width: 100%;top: -1px;"><span>ᛗᛁᛗᛁᚱ</span>
+        <div class="xHPsid" jsname="njTs3e" role="list"><div class="wZTANe" role="listitem"></div><div class="wZTANe" role="listitem"><a class="u2mfde hN1OOc EZrbnd J1raN S6Vdac active " target="_self" href="https://booktimes.github.io/sol/" guidedhelpid="classworkTab">Ask</a></div><div class="wZTANe" role="listitem"><a class="u2mfde hN1OOc EZrbnd J1raN S6Vdac " target="_self" href="https://slackedlime.github.io/Mimir/" guidedhelpid="studentTab">Tell</a></div></div>
       </div>
-    </div>
-  </div>
-    `
-    );
-    setn += 1;
-  });
-}, 1000);
+      
+      <div class="item1" style="opacity: 0;"><span>Mim</span></div>
+      <div class="item3">
+            <h1 id="ol">Ask your doubts</h1>
+            <div id="qnb" class="qnBox">
+                <textarea  placeholder="Tell me your doubts" class="tbox" id="tbox"></textarea>
+                <select  class="sel" id="sel">
+                    <option value="nono">
+                        Pick a subject
+                        </option>
+                        <option value="Maths">Mathematics</option>
+                 
+                        <option  value="English">English</option>
+                        <option  value="Biology">Biology</option>
+                        <option  value="Chemistry">Chemistry</option>
+                        <option  value="Physics">Physics</option>
+                        <option  value="Social">Social Studies</option>
+                       
+                        <option value="Computer">Computer Science</option>
+                        <option  value="French">French</option>
+                        <option  value="Hindi">Hindi</option>
+                        <option  value="Malayalam">Malayalam</option>
 
-function ansub(el) {
-  const txb = document.getElementById(`cmbox ${el.id}`);
-  ol[el.id].cm.push(user.id + ": " + txb.value);
-  send(null, true);
-  location.reload();
-}
 
-function ent(w) {
-  const h = w;
-  h.addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-      event.preventDefault();
-      document.getElementById(h.name).click();
-    }
-  });
-}
-const modsel = document.getElementById("modsel");
-const rosel = document.getElementById("rosel");
-const ossel = document.getElementById("ossel");
-const subsel = document.getElementById("subsel");
+                        
+                </select>
+                <button class="tb" id="tb" onclick="submit()">Ask</button>
+            </div>
+            <div  class="dbts" id="dbts">
+              <form class="modal hid" id="modsel">
+                <h1 style="text-align: center;">Sign In</h1>
+                <input id="ussel" type="text" class="tbox" placeholder="Username" style="height: 15px;margin-bottom: -20px; "></input>
+                <select  class="spin" id="rosel">
+                  <option value="nono">Role</option>
+                  <option value="tr">Teacher</option>
+                  <option value="sd">Student</option>
+                </select>
+                <select  class="spin hid" id="sujsel">
+                  <option value="nono">Subject</option>
+                  <option value="Maths">Mathematics</option>
+                  <option  value="English">English</option>
+                  <option  value="Biology">Biology</option>
+                  <option  value="Chemistry">Chemistry</option>
+                  <option  value="Physics">Physics</option>
+                  <option  value="Social">Social Studies</option>
+                  <option value="Computer">Computer Science</option>
+                  <option  value="French">French</option>
+                  <option  value="Hindi">Hindi</option>
+                  <option  value="Malayalam">Malayalam</option>
+                </select>
+                <select  class="spin hid" id="grsel">
+                  <option>Grade</option>
+                   <option>1</option>
+                   <option>2</option>
+                   <option>3</option>
+                   <option>4</option>
+                   <option>5</option>
+                   <option>6</option>
+                   <option>7</option>
+                   <option>8</option>
+                   <option>9</option>
+                   <option>10</option>
+                </select>
+                <input id="subsel" type="submit" class="subb" onclick="checkCookie()"></input>
+              </div>
+              <div class="overlay hid" id="ossel"></div>
+          </div>
+      </div>
 
-rosel.addEventListener("change", function () {
-  if (rosel.value == "sd") {
-    document.getElementById("grsel").classList.remove("hid");
-    document.getElementById("sujsel").classList.add("hid");
-  } else {
-    document.getElementById("grsel").classList.add("hid");
-    document.getElementById("sujsel").classList.remove("hid");
-  }
-});
-function setCookie(cname, cvalue, exdays) {
-  const d = new Date();
-  d.setTime(d.getTime() + exdays * 24 * 60 * 60 * 1000);
-  let expires = "expires=" + d.toUTCString();
-  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-}
-function getCookie(cname) {
-  let name = cname + "=";
-  let decodedCookie = decodeURIComponent(document.cookie);
-  let ca = decodedCookie.split(";");
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) == " ") {
-      c = c.substring(1);
-    }
-    if (c.indexOf(name) == 0) {
-      return c.substring(name.length, c.length);
-    }
-  }
-  return "";
-}
-function checkCookie() {
-  let id = getCookie("id");
-  if (id != "") {
-  } else {
-    send();
-    modsel.classList.remove("hid");
-    ossel.classList.remove("hid");
-    id = document.getElementById("ussel").value;
-    const gr = document.getElementById("grsel").value;
-    const suj = document.getElementById("sujsel").value;
-    const role = rosel.value;
-    if (id != "" && role != "nono") {
-      setCookie("id", id, 365);
-      setCookie("role", role, 365);
-      setCookie("suj", suj, 365);
-      setCookie("gr", gr, 365);
-    }
-  }
-}
-function delc() {
-  var cookies = document.cookie.split(";");
-  for (var i = 0; i < cookies.length; i++) {
-    var cookie = cookies[i];
-    var eqPos = cookie.indexOf("=");
-    var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
-    document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
-  }
-  setCookie("id", "", 365);
-
-  location.reload();
-}
+  </body>
+   
+</html>
+<script src="script.js" ></script>
 
